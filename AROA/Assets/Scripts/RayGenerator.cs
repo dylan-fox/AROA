175392e36a2c7261fd41531cc0f27f6df7ae9ef8
Dylan Fox <dylan-fox@users.noreply.github.com>
2020-09-01 18:59:48 -0700

    Shader Prototype
    Environmental mesh now glows green as you get nearer. Cleaned up CC Shader files.

--- AROA/Assets/Scripts/RayGenerator.cs
+++ AROA/Assets/Scripts/RayGenerator.cs
@@ -6,17 +6,22 @@ using UnityEngine;
 /// This script raycasts from the camera to a point within a certain max distance, then draws guidelines towards that point. This should help someone with poor periperal vision find the origin point.
 /// </summary>
 
-public class RayGenerator : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+namespace Microsoft.MixedReality.Toolkit { 
+
+    public class RayGenerator : MonoBehaviour
     {
+        // Start is called before the first frame update
+        void Start()
+        {
         
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
---
+++ AROA/Assets/Scripts/ShaderHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script passes the camera position to the shaders. 
+/// </summary>
+
+namespace Microsoft.MixedReality.Toolkit
+{
+    public class ShaderHandler : MonoBehaviour
+    {
+
+        private Vector3 cameraPos; //the position of the camera
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            cameraPos = Camera.main.transform.position;
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            cameraPos = Camera.main.transform.position;
+            Shader.SetGlobalVector("_CameraPos", cameraPos);
+            
+            // Note that the shaders have been modified from the CC Shader kit by commenting out _CameraPos from Properties; otherwise Set Global Vector doesn't work.
+
+        }
+    }
+}
