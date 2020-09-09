a105ce3f1d23c38e1163843588bdb078489f08d4
Dylan Fox <dylan-fox@users.noreply.github.com>
2020-09-08 18:17:32 -0700

    Guidelines & Speech Commands
    Implemented guidelines feature, as well as "Place Guidelines," "Clear Guidelines," and "Toggle Observer" speech commands.

---
+++ AROA/Assets/Scripts/Guidelines.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit
+{
+    public class Guidelines : MonoBehaviour
+    {
+
+        /// <summary>
+        /// This script raycasts from the camera to a point within a certain max distance, then draws guidelines towards that point. This should help someone with poor periperal vision find the origin point.
+        /// </summary>
+        /// 
+
+        public GameObject guidelines;
+
+
+        public void placeGuidelines()
+        {
+            //Raycasts from the camera to the mesh. If it hits, activates guidelines and places them there.
+
+            Vector3 cameraPos = Camera.main.transform.position;
+            Vector3 cameraForward = Camera.main.transform.forward;
+
+            RaycastHit hit;
+
+            if (UnityEngine.Physics.Raycast(cameraPos, cameraForward, out hit)) {
+                Debug.Log("Raycast Hit detected. Placing guidelines.");
+                guidelines.SetActive(true);
+                guidelines.transform.position = hit.point;
+                guidelines.transform.rotation = Camera.main.transform.rotation;
+                guidelines.transform.Rotate(0f, 90f, 0f);
+
+
+            }
+
+            else
+            {
+                Debug.Log("No obstacles detected.");
+            }
+
+
+        }
+
+        public void clearGuidelines()
+        {
+            guidelines.SetActive(false);
+        }
+    }
+
+}
--- AROA/Assets/Scripts/ShaderHandler.cs
+++ AROA/Assets/Scripts/ShaderHandler.cs
@@ -10,14 +10,14 @@ namespace Microsoft.MixedReality.Toolkit
 {
     public class ShaderHandler : MonoBehaviour
     {
-
+        private bool clearObservations = true;
         private Vector3 cameraPos; //the position of the camera
 
         // Start is called before the first frame update
         void Start()
         {
             cameraPos = Camera.main.transform.position;
-            
+
         }
 
         // Update is called once per frame
@@ -28,6 +28,28 @@ namespace Microsoft.MixedReality.Toolkit
             
             // Note that the shaders have been modified from the CC Shader kit by commenting out _CameraPos from Properties; otherwise Set Global Vector doesn't work.
 
+        }
+
+        public void toggleObserver()
+        {
+            //Toggles spatial observer
+            var spatialAwarenessSystem = CoreServices.SpatialAwarenessSystem;
+            if (spatialAwarenessSystem != null)
+            {
+                if (clearObservations)
+                {
+                    Debug.Log("Suspending observers.");
+                    spatialAwarenessSystem.SuspendObservers();
+                    spatialAwarenessSystem.ClearObservations();
+                    clearObservations = false;
+                }
+                else
+                {
+                    Debug.Log("Resuming observers.");
+                    spatialAwarenessSystem.ResumeObservers();
+                    clearObservations = true;
+                }
+            }
         }
     }
 }
